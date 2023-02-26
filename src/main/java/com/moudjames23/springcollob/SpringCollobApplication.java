a331b8ea481af0d0aade35265def961629ab3505
Diallo Mamoudou <moud23diallo@outlook.com>
2023-02-26 14:54:23 +0000

    Initial checkin

---
+++ src/main/java/com/moudjames23/springcollob/SpringCollobApplication.java
@@ -0,0 +1,13 @@
+package com.moudjames23.springcollob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCollobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCollobApplication.class, args);
+    }
+
+}
